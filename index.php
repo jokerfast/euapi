e929a64193fce30651a21c94055ab503059820ac
jokerfast <49236631+jokerfast@users.noreply.github.com>
2026-02-01 20:51:30 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL);
 $z_key = $_GET['keyword'];
 $z_options = array();
 $z_options[] = array('name'=>'gclid', 'value'=>$_GET['gclid']);
-include $_SERVER['DOCUMENT_ROOT'].'/api.php';
+include $_SERVER['DOCUMENT_ROOT'].'/tdsapi.php';
 if($z_bot == $z_empty && !empty($z_out)){header("Location: $z_out");}
 ?>
 
